44d80c691d8011c811f36a0d936858098043baa7
Kohta-Nakagawa <nkgwkht@gmail.com>
2021-06-27 10:12:01 +0900

    addtweetvue

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,12 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6"> </v-col>
+    <v-col cols="12" sm="8" md="6">
+      <v-btn to="/tweet" nuxt class="mx-2" fab dark large color="cyan">
+        <v-icon dark>
+          mdi-pencil
+        </v-icon>
+      </v-btn>
+    </v-col>
   </v-row>
 </template>
 
---
+++ pages/tweet.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-textarea outlined label="何か書こう！" value=""></v-textarea>
+</template>
+<script>
+export default {
+  methods: {}
+};
+</script>
